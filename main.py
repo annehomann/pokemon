e0bf9b62f3ecc9fe40fdd424b4b049e99acb00b1
Anne Homann <an.hmnn@gmail.com>
2019-08-24 16:39:35 +1000

    add list and search function

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# Jaden's solution
+# https://github.com/jadencamelot/pokemon/tree/solution
+
 from client import Client
 
 def main():
@@ -10,14 +13,34 @@ def main():
         command = input("Enter command: ")
         if command == "hello":
             print("hello")
+        elif command == "all":
+            print(c.pokemon)
         elif command == "exit":
             exit_flag = True
         elif command == "pokemon":
             n_pokemon = len(c.pokemon)
             print(f"There are {n_pokemon} pokemon")
+        elif command == "list":
+            for i in c.pokemon:
+                print(i['name'].title())
+        elif command == "search":
+            entry = input("Write the name of the pokemon you would like to know about: ")
+            for p in c.pokemon:
+                if p['name'] == entry:
+                    print(p['name'].title())
+                    print(p['height'])
+
             
+
+
 
 if __name__ == "__main__":
     main()
 
-# This is some test text
+# User can search for a pokemon via command interface
+# Commands:
+# "list" - list names of all pokemon
+# "search <pokemon> - search for a pokemon and display info about it"
+
+# Client class fetch data for a given pokemon and return it
+
